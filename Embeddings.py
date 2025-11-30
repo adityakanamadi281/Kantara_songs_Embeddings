a0b26578e94ce919e5a41a92beaa297be14f0d8e
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-11-30 21:58:57 +0530

    Embeddings

---
+++ Embeddings.py
@@ -0,0 +1,43 @@
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
+import torch
+import librosa
+
+
+
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
+
+
+
+audio_files = [r"C:\Users\adity\Kantara_songs_Embeddings\Audio\Bramhakalasha_part2.wav",
+                r"C:\Users\adity\Kantara_songs_Embeddings\Audio\Bramhakalasha.wav",
+                r"C:\Users\adity\Kantara_songs_Embeddings\Audio\karma.wav",
+                r"C:\Users\adity\Kantara_songs_Embeddings\Audio\Varaha_Roopam.wav",
+                r"C:\Users\adity\Kantara_songs_Embeddings\Audio\rebel.wav"]
+
+
+embeddings = [] 
+
+for file in audio_files:
+    print(f"Processing: {file}")
+
+    # Load one file at a time
+    audio, rate = librosa.load(file, sr=16000)
+
+    # Extract features
+    inputs = feature_extractor(audio, return_tensors="pt", sampling_rate=rate)
+
+    # Forward pass
+    with torch.no_grad():
+        outputs = model(**inputs)
+        emb = outputs.last_hidden_state.mean(dim=1)   # mean pooling
+
+    embeddings.append(emb)
+
+    print(f"Embedding shape: {emb.shape}\n")
+
+
+final_embeddings = torch.stack(embeddings)
+print("Final embeddings shape:", final_embeddings.shape)
+
+
